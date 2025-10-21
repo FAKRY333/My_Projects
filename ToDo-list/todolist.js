4a59deb312356a28823db3a1abc5c24cbeb1d905
Fakry <aaajjjdheid@gmail.com>
2025-10-22 00:32:23 +0300

    Add files via upload

---
+++ ToDo-list/todolist.js
@@ -0,0 +1,77 @@
+
+// get the elements 
+let input = document.querySelector('.input');
+let btn = document.querySelector('.btn');
+let content = document.querySelector('.content');
+// teke the tasks from the localstorge
+
+
+window.onload  = function() {
+  let tasks = localStorage.getItem('tasks');
+  tasks = JSON.parse(tasks);
+  tasks.forEach((e,i) => {
+    let div = document.createElement('div');
+    let h2 = document.createElement('h3');
+    let del = document.createElement('button');
+    let scc = document.createElement('button');
+    let buutons = document.createElement('span');
+    scc.textContent = 'تم';
+    scc.className = 'scc';
+    del.textContent = 'DELETE';
+    del.className = 'del';
+    scc.onclick = () => {
+      console.log('yes')
+      h2.classList.add('active');
+       tasks[i].Done = true; 
+       localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
+    if (tasks[i].Done === true) {
+      h2.className = 'active';
+    }
+    del.addEventListener('click' ,() => {
+     let acc =  confirm('Are you sure ?');
+     console.log(acc)
+     if (acc) {
+       div.remove();
+       tasks.splice(i,1);
+       localStorage.setItem('tasks', JSON.stringify(tasks))
+      } else {
+        console.log('no')
+      }
+    })
+    h2.textContent = e.Task ;
+    buutons.appendChild(del);
+    buutons.appendChild(scc);
+    div.appendChild(buutons);
+    div.appendChild(h2);
+    content.appendChild(div);
+  });
+}
+
+
+// 
+
+
+// we hava emty array
+btn.addEventListener('click', (e) => {
+  // function
+    function getFromlocal() {
+        let tasks = localStorage.getItem('tasks');
+        return  tasks ? JSON.parse(tasks) : [] ;
+    }
+// fucntion 
+  function addTolocal() {
+    let tasks = getFromlocal(); // tasks = [];
+    if (input.value.trim() === '') return ;
+    tasks.push({
+      Id: +(Math.random() * 10).toFixed(),
+      Task:input.value,
+      Done:false
+    })
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+  addTolocal();
+  location.reload();
+})
+let a = [4,5,3];
+
